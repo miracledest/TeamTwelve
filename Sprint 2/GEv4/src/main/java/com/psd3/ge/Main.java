f4d29f2a3be3b36f80b29661067e065e3887fa49
miracledest <zhiyong-@hotmail.com>
2013-12-09 00:26:37 +0800

    Version 4 - Implement the 3 views "Today", "This Week", "All"

---
+++ Sprint 2/GEv4/src/main/java/com/psd3/ge/Main.java
@@ -0,0 +1,13 @@
+package com.psd3.ge;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static boolean login = false;
+	public static String userType = "";
+	public static String userName = "";
+	
+	public static ArrayList<Session> mySession = new ArrayList<Session>();
+
+}
